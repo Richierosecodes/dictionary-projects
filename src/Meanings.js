eb2552900e07aa43b20d7290b3996ca7880f5e36
Richierosecodes <roselynolayinkaakintobi@gmail.com>
2024-01-13 03:01:31 +0100

    Added synonyms component

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,16 +1,20 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meanings(props) {
     console.log(props.meanings);
     return(
         <div className = "Meanings">
             <h3>{props.meanings.partOfSpeech}</h3>
-            
-                <p>
+                         <p>
+                            
                     {props.meanings.definition}
                     <br />
                     <em>{props.meanings.example}</em>
+                    <br />
+                    <Synonyms synonyms = {definition.synonyms} />
+                    
                 </p>
-        </div>
+                 </div>
     );  
 }
